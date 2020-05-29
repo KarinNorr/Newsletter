2b1ee61ae74f2a3edae9e4e9069013038bf7a15a
Karin Norrlander <karin@barprego.se>
2020-05-29 11:03:24 +0200

    Post newUser

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -15,8 +15,33 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST user */
-router.post('/', function(req, res){
+router.post('/newUser', function(request, response){
+  fs.readFile("users.json", (err, data) => {
+    if(err) throw err; 
+    var users = JSON.parse(data);
+    
+    newUser = {
+      "id": request.body.id,
+      "userName": request.body.userName,
+      "userEmail": request.body.userEmail,
+      "subscribe": request.body.isSubscriber
+    }
 
+    users.push(newUser);
+    var saveUsers = JSON.stringify(users, null, 2);
+    fs.writeFile("users.json", saveUsers, (err, data) => {
+        if(err) throw err;
+    });
+
+    response.send("Du är nu registrerad");
+    //skicka tillbaks objektet istället
+  })
+  
+});
+
+    
+  //kryptera lösenordet
+  //kalla på metoden som krypterar lösenordet 
   //fånga informationen från ett formulär
   // Skickas med json til post
   //Kryptera lösenord innan spara
@@ -24,19 +49,22 @@ router.post('/', function(req, res){
   //writefile 
   //
 
-});
+  
+
 
 /* GET user */
-router.post('/', function(req, res){
+// router.post('/', function(req, res){
 
-  //Fånga användare och lösenord från inloggning
-  //skickas med json 
-  //Jämför att lösenordet stämmer
-  //Visa inloggad 
-  //Vad blir svaret när det stämmer
-  //Svaret när det ej stämmer
+//   //Fånga användare och lösenord från inloggning
+//   //skickas med json 
+//   //Jämför att lösenordet stämmer
+//   //Visa inloggad 
+//   //Vad blir svaret när det stämmer
+//   //Svaret när det ej stämmer
 
-});
+// });
 
 
 module.exports = router;
+
+//metod som krypterar password
