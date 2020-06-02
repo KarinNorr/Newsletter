7da9aae36a0e32f7169024da3a395e90771396dc
Karin Norrlander <karin@barprego.se>
2020-06-02 16:37:17 +0200

    changes loggin

--- Frontend/my-app/src/Components/LoggInForm.js
+++ Frontend/my-app/src/Components/LoggInForm.js
@@ -4,16 +4,24 @@ class Loggin extends React.Component{
 
     constructor(props){
         super(props);
-        this.state = { valueName: '', valueUserid: '', valueEmail: '', valuePassword: '' }
+        this.state = { userName: '', usserId: '', userEmail: '', userPassword: '' };
         
+         this.handleChange = this.handleChange.bind(this);
     }
 
-    handleChange = (event) => {
+   
 
-        this.setState = ({userName: event.target.userName })
+
+    handleChange = (event) => {
+        console.log(event.target);
+        //const userName = event.target.userName.value;
+        console.log("Användarnamn:");
+        //console.log(userName);
+
+        this.setState = ({userName: event.target.value })
         // console.log(event.target);
         // this.setState( {valueName:event.target.name.value })
-        // console.log( {valueName:event.target.value} );
+        console.log( {userName:event.target.value} );
         // this.setState( {valueUserid:event.target.value })
         // this.setState( {valueEmail:event.target.value })
         // this.setState( {valuePassword:event.target.password.value })
@@ -25,7 +33,7 @@ class Loggin extends React.Component{
         // //kör callback tillbaks till parent igen
         // //med datan som behövs
         // //göra en fetch för att hämta informationen 
-        // this.props.getCurrentUser(this.state.valueName);
+        this.props.getCurrentUser(this.state.userName.value);
         event.preventDefault();
     }
 
@@ -40,7 +48,7 @@ class Loggin extends React.Component{
                 <form onSubmit = { this.handleSubmit}>
                     <label>
                         Användarnamn:
-                        <input type="text" name="userName" value = { this.state.userName.value } onChange = { this.handleChange } />
+                        <input type="text" name="userName" value ={ this.state.userName.value } onChange = { this.handleChange } />
                         Lösenord:
                         <input type="text" name="userPassword" value = { this.state.userPassword.value } onChange = { this.handleChange } />
                     </label>
